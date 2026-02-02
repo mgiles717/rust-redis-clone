98eb287b2dc88191ef433cfe60fbaaa2153fb092
Max Giles <mgiles717@gmail.com>
2026-02-02 00:43:25 +0000

    feat: build ipv4 obj

--- redis-clone/src/main.rs
+++ redis-clone/src/main.rs
@@ -1,3 +1,12 @@
+use std::net::{TcpListener, TcpStream};
+use std::net::{Ipv4Addr};
+use std::io::{Read, Write};
+
+fn init_server() -> Ipv4Addr {
+    let localhost = Ipv4Addr::new(127, 0, 0, 1);
+    return localhost;
+}
+
 fn main() {
-    println!("Hello, world!");
+    init_server();
 }
