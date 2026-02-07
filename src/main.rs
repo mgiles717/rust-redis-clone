a20d38e5e7dd4b13955323d517c61c329c646a16
Max Giles <mgiles717@gmail.com>
2026-02-07 18:54:03 +0000

    refactor: split server logic into separate module

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,6 @@
-use std::net::{TcpListener, TcpStream};
-use std::io::{Write, Read, Result};
-
-fn read_from_stream(stream: &mut TcpStream){
-    let mut buffer = [0; 1024];
-
-    match stream.read(&mut buffer) {
-        Ok(0) => println!("Connection closed"),
-        Ok(n) => {
-            let data = String::from_utf8_lossy(&buffer[..n]);
-            println!("Received: {}", data);
-        }
-        Err(e) => eprint!("Error: {}", e),
-    }
-}
-
-fn handler(mut stream: TcpStream) -> Result<()> {
-    read_from_stream(&mut stream);
-    
-    stream.write_all(b"Hello World!")?;
-    stream.flush()?;
-    Ok(())
-}
-
-fn init_server() -> Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                let _ = handler(stream);
-                println!("Connection handled!");
-            }
-            Err(e) => {
-                eprintln!("Connection failed: {}", e);
-            }
-        }
-    }
-
-    Ok(())
-}
+mod server;
 
 fn main() {
     // Need to handle Result<()> for this 
-    let _ = init_server();
+    let _ = server::init_server();
 }
