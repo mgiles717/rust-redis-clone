66f1bc3da1b1c5141b2f940ca5ad64546d588763
Max Giles <mgiles717@gmail.com>
2026-02-07 04:28:24 +0000

    feat: server now processes data received from client

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
 use std::net::{TcpListener, TcpStream};
-use std::io::{Write};
-use std::io::{Result};
+use std::io::{Write, Read, Result};
+
+fn read_from_stream(stream: &mut TcpStream){
+    let mut buffer = [0; 1024];
+
+    match stream.read(&mut buffer) {
+        Ok(0) => println!("Connection closed"),
+        Ok(n) => {
+            let data = String::from_utf8_lossy(&buffer[..n]);
+            println!("Received: {}", data);
+        }
+        Err(e) => eprint!("Error: {}", e),
+    }
+}
 
 fn handler(mut stream: TcpStream) -> Result<()> {
+    read_from_stream(&mut stream);
+    
     stream.write_all(b"Hello World!")?;
     stream.flush()?;
     Ok(())
